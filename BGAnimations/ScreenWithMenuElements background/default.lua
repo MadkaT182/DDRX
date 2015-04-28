2a647ee5d5f3aa9e9a7e9de82e280aa68dd932a1
unknown <madcat182@gmail.com>
2015-04-28 15:30:28 -0500

    Added mainbg video

--- BGAnimations/ScreenWithMenuElements background/default.lua
+++ BGAnimations/ScreenWithMenuElements background/default.lua
@@ -1,22 +1,21 @@
 local t = Def.ActorFrame {};
 
 t[#t+1] = Def.ActorFrame {
-  FOV=90;
-  InitCommand=cmd(Center);
-	Def.Quad {
-		InitCommand=cmd(scaletoclipped,SCREEN_WIDTH,SCREEN_HEIGHT);
-		OnCommand=cmd(diffuse,color("#FFCB05");diffusebottomedge,color("#F0BA00"));
-	};
+  -- FOV=90;
+  -- InitCommand=cmd(Center);
+	-- Def.Quad {
+	-- 	InitCommand=cmd(scaletoclipped,SCREEN_WIDTH,SCREEN_HEIGHT);
+	-- 	OnCommand=cmd(diffuse,color("#FFCB05");diffusebottomedge,color("#F0BA00"));
+	-- };
 	Def.ActorFrame {
-		InitCommand=cmd(hide_if,hideFancyElements;);
-		LoadActor("_checkerboard") .. {
-			InitCommand=cmd(rotationy,0;rotationz,0;rotationx,-90/4*3.5;zoomto,SCREEN_WIDTH*2,SCREEN_HEIGHT*2;customtexturerect,0,0,SCREEN_WIDTH*4/256,SCREEN_HEIGHT*4/256);
-			OnCommand=cmd(texcoordvelocity,0,0.25;diffuse,color("#ffd400");fadetop,1);
+		--InitCommand=cmd(hide_if,hideFancyElements;);
+		LoadActor("BGA.avi") ..{
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoomtowidth,SCREEN_WIDTH;zoomtoheight,SCREEN_HEIGHT);
 		};
 	};
-	LoadActor("_particleLoader") .. {
-		InitCommand=cmd(x,-SCREEN_CENTER_X;y,-SCREEN_CENTER_Y;hide_if,hideFancyElements;);
-	};
+	-- LoadActor("_particleLoader") .. {
+	-- 	InitCommand=cmd(x,-SCREEN_CENTER_X;y,-SCREEN_CENTER_Y;hide_if,hideFancyElements;);
+	-- };
 --[[ 	LoadActor("_particles") .. {
 		InitCommand=cmd(x,-SCREEN_CENTER_X;y,-SCREEN_CENTER_Y);
 	}; --]]
