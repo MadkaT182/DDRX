e0d1c0624b8c1c5247663e52f85163cd8f28d935
unknown <madcat182@gmail.com>
2015-04-30 18:10:02 -0500

    Removed life and timming difficult and version info

--- BGAnimations/ScreenTitleMenu decorations.lua
+++ BGAnimations/ScreenTitleMenu decorations.lua
@@ -14,11 +14,6 @@ t[#t+1] = Def.ActorFrame {
 };
 
 t[#t+1] = StandardDecorationFromFileOptional("Footer","Footer");
-t[#t+1] = StandardDecorationFromFileOptional("Logo","Logo");
-t[#t+1] = StandardDecorationFromFileOptional("VersionInfo","VersionInfo");
-t[#t+1] = StandardDecorationFromFileOptional("CurrentGametype","CurrentGametype");
-t[#t+1] = StandardDecorationFromFileOptional("LifeDifficulty","LifeDifficulty");
-t[#t+1] = StandardDecorationFromFileOptional("TimingDifficulty","TimingDifficulty");
 t[#t+1] = StandardDecorationFromFileOptional("NetworkStatus","NetworkStatus");
 t[#t+1] = StandardDecorationFromFileOptional("SystemDirection","SystemDirection");
 
