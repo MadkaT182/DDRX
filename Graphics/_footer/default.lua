96822814b46e6fe8f56d065524344a0bb81fd981
unknown <madcat182@gmail.com>
2015-04-29 22:49:36 -0500

    Clean screen music select header and fixed banner position in screen

--- Graphics/_footer/default.lua
+++ Graphics/_footer/default.lua
@@ -5,7 +5,8 @@ return Def.ActorFrame {
 	};
 
 	LoadActor( "command_bg" )..{
-		OnCommand=cmd(x,SCREEN_CENTER_X+283;y,SCREEN_CENTER_Y+210;addx,272;sleep,0.033;decelerate,0.233;addx,-272);
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y);
+		--OnCommand=cmd(x,SCREEN_CENTER_X+283;y,SCREEN_CENTER_Y+210;addx,272;sleep,0.033;decelerate,0.233;addx,-272);
 		OffCommand=cmd(linear,0.4;addx,236;linear,0;diffusealpha,0);
 	};
 }
