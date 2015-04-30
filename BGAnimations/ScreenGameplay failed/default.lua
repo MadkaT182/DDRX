9ad00ba2becde30189ca62e07e7bdca62df035a2
unknown <madcat182@gmail.com>
2015-04-29 23:29:44 -0500

    Added failed screen

--- BGAnimations/ScreenGameplay failed/default.lua
+++ BGAnimations/ScreenGameplay failed/default.lua
@@ -1,31 +1,41 @@
-local longFail = ThemePrefs.Get("LongFail");
-
 local t = Def.ActorFrame{};
 
-if longFail then
-	t[#t+1] = Def.Quad{
-		InitCommand=cmd(FullScreen;diffuse,color("1,0,0,0");blend,Blend.Multiply);
-		OnCommand=cmd(decelerate,1.25;diffuse,color("0.75,0,0,0.75");linear,7;diffuse,color("0,0,0,1");sleep,1.25;linear,1;diffuse,color("1,0,0,1");decelerate,2;diffuse,color("0,0,0,1"));
-	};
-	t[#t+1] = Def.Quad{
-		InitCommand=cmd(FullScreen;diffuse,color("1,1,1,1");diffusealpha,0);
-		OnCommand=cmd(finishtweening;diffusealpha,1;decelerate,1.25;diffuse,color("1,0,0,0"));
-	};
-	t[#t+1] = LoadActor(THEME:GetPathS( "ScreenGameplayAlternate", "failed" ) ) .. {
+t[#t+1] = Def.ActorFrame {
+	OnCommand=function(self)
+		if GAMESTATE:IsExtraStage() then
+			self:diffuse(color("#ff5aec"));
+		elseif GAMESTATE:IsExtraStage2() then
+			self:diffuse(color("#ff5a63"));
+		end;
+	end;
+
+    LoadActor("../failed.mp3") .. {
 		StartTransitioningCommand=cmd(play);
 	};
-else
-	t[#t+1] = Def.Quad{
-		InitCommand=cmd(FullScreen;diffuse,color("1,0,0,0");blend,Blend.Multiply);
-		OnCommand=cmd(smooth,1;diffuse,color("0.75,0,0,0.75");decelerate,2;diffuse,color("0,0,0,1"));
+
+	LoadActor( "../_shared background" )..{
+		OnCommand=cmd(addy,999;sleep,3;addy,-999);
 	};
-	t[#t+1] = Def.Quad{
-		InitCommand=cmd(FullScreen;diffuse,color("1,1,1,1");diffusealpha,0);
-		OnCommand=cmd(finishtweening;diffusealpha,1;decelerate,1.25;diffuse,color("1,0,0,0"));
+
+    LoadActor("cage")..{
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-360;zoom,1.01);
+		OnCommand=cmd(zoomx,1.41;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;addy,-480;sleep,0.000;accelerate,0.316;addy,480;sleep,4;diffusealpha,1);
 	};
-	t[#t+1] = LoadActor(THEME:GetPathS( Var "LoadingScreen", "failed" ) ) .. {
-		StartTransitioningCommand=cmd(play);
+
+	LoadActor( "../door1" )..{
+		OnCommand=cmd(zoomx,1.41;x,SCREEN_CENTER_X-202;y,SCREEN_CENTER_Y;addx,-320;sleep,0.000;sleep,0.316;linear,0.2;addx,320;sleep,1.167;sleep,3.383;linear,0.2;addx,-320);
 	};
-end;
+
+	LoadActor( "../door2" )..{
+		OnCommand=cmd(zoomx,1.41;x,SCREEN_CENTER_X+202;y,SCREEN_CENTER_Y;addx,320;sleep,0.000;sleep,0.316;linear,0.2;addx,-320;sleep,1.167;sleep,3.383;linear,0.2;addx,320);
+	};
+};
+
+
+t[#t+1] = Def.ActorFrame {
+	LoadActor("Failed") .. {
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;diffusealpha,0;rotationz,-360;sleep,0.000;sleep,0.233;accelerate,0.316;rotationz,0;diffusealpha,1;accelerate,0.05;zoom,1.6;decelerate,0.083;zoom,0.95;decelerate,0.016;zoom,1;sleep,3.1;linear,0.166;addy,30;diffusealpha,0);
+	};
+};
 
 return t;
