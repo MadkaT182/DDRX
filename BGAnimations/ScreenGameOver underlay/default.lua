ae52b2631f54d44686301b5dece581e6e425e16d
unknown <madcat182@gmail.com>
2015-05-03 18:38:06 -0500

    Removed string from gameover screen

--- BGAnimations/ScreenGameOver underlay/default.lua
+++ BGAnimations/ScreenGameOver underlay/default.lua
@@ -22,8 +22,5 @@ return Def.ActorFrame {
 	LoadActor(THEME:GetPathG("ScreenGameOver","gameover"))..{
 		InitCommand=cmd(y,-16;shadowlength,2)
 	},
-	LoadFont("Common Normal")..{
-		Text=ScreenString("Play again soon!"),
-		InitCommand=cmd(y,36;shadowlength,2)
-	}
+
 }
