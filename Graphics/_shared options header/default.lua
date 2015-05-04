c21387d0b4a8db9920f35ac6594fbb9390496bf5
unknown <madcat182@gmail.com>
2015-05-04 09:13:52 -0500

    Arranged shared header objects

--- Graphics/_shared options header/default.lua
+++ Graphics/_shared options header/default.lua
@@ -1,11 +1,11 @@
 return Def.ActorFrame {
-	-- LoadActor( "header" )..{
-	-- 	OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-240;vertalign,top;zoomx, 1.34;addy,-24;linear,0.133;addy,24);
-	-- 	OffCommand=cmd(linear,0.133;addy,-56);
-	-- };
+	LoadActor( "header" )..{
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_TOP;vertalign,top;zoomx, 1.34;addy,-24;linear,0.133;addy,24);
+		OffCommand=cmd(linear,0.133;addy,-56);
+	};
 
-	-- LoadActor( "frame" )..{
-	-- 	OnCommand=cmd(x,SCREEN_CENTER_X-159;y,SCREEN_CENTER_Y-232;vertalign,top;horizalign,right;zoom,0;sleep,0.283;linear,0.233;zoom,1.05;linear,0.066;zoom,1);
-	-- 	OffCommand=cmd(linear,0.133;addy,-62);
-	-- };
+	LoadActor( "frame" )..{
+		OnCommand=cmd(x,SCREEN_LEFT+289;y,SCREEN_TOP-14;vertalign,top;horizalign,right;zoom,0;sleep,0.283;linear,0.233;zoom,1.05;linear,0.066;zoom,1);
+		OffCommand=cmd(linear,0.133;addy,-62);
+	};
 }
