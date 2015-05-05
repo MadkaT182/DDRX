6b8b53e18e174d2363dde53891663314a6fb57c7
unknown <madcat182@gmail.com>
2015-05-04 23:13:02 -0500

    Removed graph from results screen

--- BGAnimations/ScreenEvaluation decorations/default.lua
+++ BGAnimations/ScreenEvaluation decorations/default.lua
@@ -1,34 +1,3 @@
-local function GraphDisplay( pn )
-	local t = Def.ActorFrame {
-		LoadActor( THEME:GetPathG("_GraphDisplay","overlay")) .. {
-			InitCommand=cmd(y,6);
-		};
-		Def.GraphDisplay {
-			InitCommand=cmd(Load,"GraphDisplay";);
-			BeginCommand=function(self)
-				local ss = SCREENMAN:GetTopScreen():GetStageStats();
-				self:Set( ss, ss:GetPlayerStageStats(pn) );
-				self:player( pn );
-			end
-		};
-	};
-	return t;
-end
-
-local function ComboGraph( pn )
-	local t = Def.ActorFrame {
-		Def.ComboGraph {
-			InitCommand=cmd(Load,"ComboGraph";);
-			BeginCommand=function(self)
-				local ss = SCREENMAN:GetTopScreen():GetStageStats();
-				self:Set( ss, ss:GetPlayerStageStats(pn) );
-				self:player( pn );
-			end
-		};
-	};
-	return t;
-end
-
 local function PercentScore( pn )
 	local t = LoadFont("Common normal")..{
 		InitCommand=cmd(zoom,0.625;shadowlength,1;player,pn);
@@ -69,18 +38,6 @@ end
 local t = LoadFallbackB();
 
 t[#t+1] = StandardDecorationFromFileOptional("StageDisplay","StageDisplay");
-
-if ShowStandardDecoration("GraphDisplay") and GAMESTATE:GetPlayMode() ~= "PlayMode_Rave" then
-	for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
-		t[#t+1] = StandardDecorationFromTable( "GraphDisplay" .. ToEnumShortString(pn), GraphDisplay(pn) );
-	end
-end
-
-if ShowStandardDecoration("ComboGraph") and GAMESTATE:GetPlayMode() ~= "PlayMode_Rave" then
-	for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
-		t[#t+1] = StandardDecorationFromTable( "ComboGraph" .. ToEnumShortString(pn), ComboGraph(pn) );
-	end
-end
 
 if ShowStandardDecoration("StepsDisplay") then
 	for pn in ivalues(PlayerNumber) do
