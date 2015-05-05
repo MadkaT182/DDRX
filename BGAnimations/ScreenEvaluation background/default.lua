d5a6452a24808f4f58a96110bfeddb61a8ea265f
unknown <madcat182@gmail.com>
2015-05-05 11:21:57 -0500

    Arrange position of header and footer for screen evaluation

--- BGAnimations/ScreenEvaluation background/default.lua
+++ BGAnimations/ScreenEvaluation background/default.lua
@@ -4,12 +4,12 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor( "../Common ScreenStages/top_stage" )..{
-		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-212;zoomx,1.34;addy,-56;linear,0.133;addy,56);
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_TOP+12;zoomx,1.34;addy,-56;linear,0.133;addy,56);
 		OffCommand=cmd(linear,0.133;addy,-56);
 	};
 
 	LoadActor( "../Common ScreenStages/bottom_stage" )..{
-		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+229;zoomx,1.34;addy,24;linear,0.133;addy,-24);
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_BOTTOM-12;zoomx,1.34;addy,24;linear,0.133;addy,-24);
 		OffCommand=cmd(linear,0.133;addy,24);
 	};
 
