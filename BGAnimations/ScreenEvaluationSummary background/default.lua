fbed3e57d3202a31db622768a686d29587053589
unknown <madcat182@gmail.com>
2015-05-05 11:30:47 -0500

    Added header and footer to evaluation summary screen

--- BGAnimations/ScreenEvaluationSummary background/default.lua
+++ BGAnimations/ScreenEvaluationSummary background/default.lua
@@ -1,4 +1,15 @@
 return Def.ActorFrame {
 	LoadActor( "../_shared background" )..{
 	};
+
+	LoadActor( "../Common ScreenStages/top_stage" )..{
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_TOP+12;zoomx,1.34;addy,-56;linear,0.133;addy,56);
+		OffCommand=cmd(linear,0.133;addy,-56);
+	};
+
+	LoadActor( "../Common ScreenStages/bottom_stage" )..{
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_BOTTOM-12;zoomx,1.34;addy,24;linear,0.133;addy,-24);
+		OffCommand=cmd(linear,0.133;addy,24);
+	};
+
 }
