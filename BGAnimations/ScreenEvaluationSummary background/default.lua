d1b39c08477fd06ef93ddc9595de20b0d4068a78
unknown <madcat182@gmail.com>
2015-05-05 11:53:25 -0500

    Added header frame and clock bg to screen evaluation summary

--- BGAnimations/ScreenEvaluationSummary background/default.lua
+++ BGAnimations/ScreenEvaluationSummary background/default.lua
@@ -7,6 +7,14 @@ return Def.ActorFrame {
 		OffCommand=cmd(linear,0.133;addy,-56);
 	};
 
+	LoadActor( "../headerFrame" )..{
+		OnCommand=cmd(x,SCREEN_CENTER_X+147;y,SCREEN_TOP-16;vertalign,top;horizalign,right;zoom,0;linear,0.233;zoom,1.05;linear,0.066;zoom,1);
+		OffCommand=cmd(linear,0.133;addy,-62);
+	};
+
+	LoadActor("../../Graphics/timerBG")..{
+	};
+
 	LoadActor( "../Common ScreenStages/bottom_stage" )..{
 		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_BOTTOM-12;zoomx,1.34;addy,24;linear,0.133;addy,-24);
 		OffCommand=cmd(linear,0.133;addy,24);
