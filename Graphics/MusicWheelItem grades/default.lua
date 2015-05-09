4b2a4b4be5d4d0e3b93779f4ef3c59a108bce1d1
unknown <madcat182@gmail.com>
2015-05-09 12:24:12 -0500

    Changed grade font from banner

--- Graphics/MusicWheelItem grades/default.lua
+++ Graphics/MusicWheelItem grades/default.lua
@@ -25,7 +25,7 @@ local grades = {
 		self:setstate(state);
 	end;
 }; --]]
-local t = LoadFont("Common Normal") .. {
+local t = LoadFont("_shared2") .. {
 	InitCommand=cmd(zoom,0.75;shadowlength,1;strokecolor,Color("Black"));
 	ShowCommand=cmd(stoptweening;bounceend,0.15;zoomy,0.75);
 	HideCommand=cmd(stoptweening;bouncebegin,0.15;zoomy,0);
@@ -35,9 +35,9 @@ local t = LoadFont("Common Normal") .. {
 		local gradeString = THEME:GetString("Grade",string.sub(sGrade,7));
 
 		self:settext(gradeString);
-		self:diffuse(PlayerColor(pnPlayer));
-		self:diffusetopedge(BoostColor(PlayerColor(pnPlayer),1.5));
-		self:strokecolor(BoostColor(PlayerColor(pnPlayer),0.25));
+		--self:diffuse(PlayerColor(pnPlayer));
+		--self:diffusetopedge(BoostColor(PlayerColor(pnPlayer),1.5));
+		--self:strokecolor(BoostColor(PlayerColor(pnPlayer),0.25));
 		
 --[[ 		if sGrade == "Grade_NoTier" then
 			self:playcommand("Hide");
@@ -48,26 +48,3 @@ local t = LoadFont("Common Normal") .. {
 };
 
 return t;
-
--- (c) 2007 Glenn Maynard
--- All rights reserved.
--- 
--- Permission is hereby granted, free of charge, to any person obtaining a
--- copy of this software and associated documentation files (the
--- "Software"), to deal in the Software without restriction, including
--- without limitation the rights to use, copy, modify, merge, publish,
--- distribute, and/or sell copies of the Software, and to permit persons to
--- whom the Software is furnished to do so, provided that the above
--- copyright notice(s) and this permission notice appear in all copies of
--- the Software and that both the above copyright notice(s) and this
--- permission notice appear in supporting documentation.
--- 
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
--- OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
--- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
--- THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
--- INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
--- OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
--- OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
--- OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
--- PERFORMANCE OF THIS SOFTWARE.
