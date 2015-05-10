2ceb09af1cd0965091d173eee72c4795781fbd25
unknown <madcat182@gmail.com>
2015-05-09 23:55:28 -0500

    Added sound to intro movie

--- BGAnimations/ScreenIntroMovie background/default.lua
+++ BGAnimations/ScreenIntroMovie background/default.lua
@@ -1,10 +1,11 @@
 return Def.ActorFrame {
 	LoadActor( "Intro" )..{
-		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoomtowidth,SCREEN_WIDTH;zoomtoheight,SCREEN_HEIGHT);
+		InitCommand=cmd(FullScreen);
 	};
 
-	LoadActor( "../_black" )..{
-		BeginCommand=cmd(StretchTo,0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
-		OnCommand=cmd(diffusealpha,0;sleep,27.0;diffusealpha,0);
+	LoadActor( "music" )..{
+		OnCommand=cmd(play);
+		OffCommand=cmd(stop);
 	};
+
 }
