dea071bd9de78f1e4ca4ffea345e642dea99fe1d
unknown <madcat182@gmail.com>
2015-05-10 21:41:03 -0500

    Added song name background to screen gameplay

--- BGAnimations/ScreenGameplay decorations/default.lua
+++ BGAnimations/ScreenGameplay decorations/default.lua
@@ -224,7 +224,10 @@ for pn in ivalues(PlayerNumber) do
 end;
 
 
-t[#t+1] = StandardDecorationFromFileOptional("BPMDisplay","BPMDisplay");
+t[#t+1] = 	LoadActor( "../../Graphics/ScreenGameplay song info" )..{
+	InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_BOTTOM-42;zoom,1);
+};
+
 t[#t+1] = StandardDecorationFromFileOptional("StageDisplay","StageDisplay");
 t[#t+1] = StandardDecorationFromFileOptional("SongTitle","SongTitle");
 
