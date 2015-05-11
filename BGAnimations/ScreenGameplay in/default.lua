b511dde2a01686ab938c6b3686941e077b7dd7de
unknown <madcat182@gmail.com>
2015-05-10 22:06:49 -0500

    Added screen gameplay in

--- BGAnimations/ScreenGameplay in/default.lua
+++ BGAnimations/ScreenGameplay in/default.lua
@@ -12,4 +12,13 @@ t[#t+1] = Def.Sprite {
 		self:diffusealpha(0)
 	end;
 };
+
+t[#t+1] = LoadActor( "../door1" )..{
+		OnCommand=cmd(zoomx,1.34;x,SCREEN_CENTER_X-214;y,SCREEN_CENTER_Y;sleep,0.0000;linear,0.2;addx,-999);
+};
+
+t[#t+1] = LoadActor( "../door2" )..{
+		OnCommand=cmd(zoomx,1.34;x,SCREEN_CENTER_X+214;y,SCREEN_CENTER_Y;sleep,0.0000;linear,0.2;addx,999);
+};
+
 return t;
