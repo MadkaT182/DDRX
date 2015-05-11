da6f699e2fc509620d1f318350ef6a90a6230cfd
unknown <madcat182@gmail.com>
2015-05-10 21:12:24 -0500

    Arranged how to play screen background music

--- BGAnimations/ScreenAlbums background/default.lua
+++ BGAnimations/ScreenAlbums background/default.lua
@@ -1,5 +1,9 @@
 return Def.ActorFrame {
 
+	LoadActor( "bgm" )..{
+		OnCommand=cmd(play);
+		OffCommand=cmd(stop);
+	};
 	LoadActor( "howtoplay" )..{
 		BeginCommand=cmd(StretchTo,0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
 		OnCommand=cmd(Center;FullScreen);
