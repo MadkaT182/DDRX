cc505e20f350eaeee83c7ba5d4128f8dce142c63
unknown <madcat182@gmail.com>
2015-05-10 21:28:52 -0500

    Arranged screen company background music

--- BGAnimations/ScreenCompany background/default.lua
+++ BGAnimations/ScreenCompany background/default.lua
@@ -1,4 +1,10 @@
 return Def.ActorFrame {
+
+	LoadActor( "bgm" )..{
+		OnCommand=cmd(play);
+		OffCommand=cmd(stop);
+	};
+
 	LoadActor( "konami" )..{
 		OnCommand=cmd(Center;FullScreen;diffusealpha,0;decelerate,0.6;diffusealpha,1;sleep,5.2;accelerate,0.6;diffusealpha,0);
 	};
