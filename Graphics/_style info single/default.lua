b91a7ce43f49c86891321c15086fd28ff4dc96cc
unknown <madcat182@gmail.com>
2015-05-15 23:27:06 -0500

    Update position for select style info

--- Graphics/_style info single/default.lua
+++ Graphics/_style info single/default.lua
@@ -1,6 +1,6 @@
 return Def.ActorFrame {
 	LoadActor( "info" )..{
-		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-350);
+		InitCommand=cmd(x,SCREEN_CENTER_X-21;y,SCREEN_CENTER_Y-335);
 		OnCommand=cmd();
 	};
 }
--- Graphics/_style info versus/default.lua
+++ Graphics/_style info versus/default.lua
@@ -1,6 +1,6 @@
 return Def.ActorFrame {
 	LoadActor( "info" )..{
-		InitCommand=cmd(x,SCREEN_CENTER_X-83;y,SCREEN_CENTER_Y-350);
+		InitCommand=cmd(x,SCREEN_CENTER_X-103;y,SCREEN_CENTER_Y-335);
 		OnCommand=cmd();
 	};
 }
