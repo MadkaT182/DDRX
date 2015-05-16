90304fa369cefa52a5ad9a98c36649ba02569135
unknown <madcat182@gmail.com>
2015-05-16 10:02:16 -0500

    Updated position of screen select style elements

--- BGAnimations/ScreenSelectStyle underlay/default.lua
+++ BGAnimations/ScreenSelectStyle underlay/default.lua
@@ -1,13 +1,16 @@
 return Def.ActorFrame {
 	LoadActor("graysingle")..{
-	InitCommand=cmd(x,SCREEN_CENTER_X+3;y,SCREEN_CENTER_Y+40);
+	InitCommand=cmd(x,SCREEN_CENTER_X+31;y,SCREEN_CENTER_Y+40);
+	OffCommand=cmd(linear,0.1175;zoomx,0);
 };
 
 	LoadActor("grayversus")..{
-	InitCommand=cmd(x,SCREEN_CENTER_X+86;y,SCREEN_CENTER_Y+40);
+	InitCommand=cmd(x,SCREEN_CENTER_X+113;y,SCREEN_CENTER_Y+40);
+	OffCommand=cmd(linear,0.1175;zoomx,0);
 };
 
 	LoadActor("graydouble")..{
-	InitCommand=cmd(x,SCREEN_CENTER_X+166;y,SCREEN_CENTER_Y+40);
+	InitCommand=cmd(x,SCREEN_CENTER_X+195;y,SCREEN_CENTER_Y+40);
+	OffCommand=cmd(linear,0.1175;zoomx,0);
 };
 }
--- Graphics/ScreenSelectStyle Icon.lua
+++ Graphics/ScreenSelectStyle Icon.lua
@@ -20,7 +20,8 @@ t[#t+1] = Def.ActorFrame {
 		DisabledCommand=cmd(diffuse,color("0.0,0.0,0.0,0"));
 		EnabledCommand=cmd(diffuse,color("1,1,1,1"));
 
-		OnCommand=cmd(x,-290;y,-40;zoomy,0;decelerate,0.35;zoomy,1);
+		--OnCommand=cmd(x,-290;y,-40;zoomy,0;decelerate,0.35;zoomy,1);
+		OnCommand=cmd(x,SCREEN_LEFT-290;y,SCREEN_TOP-40;zoomy,0;decelerate,0.35;zoomy,1);
 		OffCommand=cmd(linear,0.1175;zoomy,0);
 	};
 };
@@ -32,7 +33,7 @@ t[#t+1] = Def.ActorFrame {
 		DisabledCommand=cmd(diffuse,color("0.0,0.0,0.0,0"));
 		EnabledCommand=cmd(diffuse,color("1,1,1,1"));
 		--OnCommand=cmd(zoom,0;decelerate,0.35;zoom,1;);
-		OnCommand=cmd(zoom,1);
+		OnCommand=cmd(x,SCREEN_LEFT+28;y,SCREEN_TOP;zoom,1);
 		OffCommand=cmd(linear,0.1175;zoomx,0);
 	};
 };
