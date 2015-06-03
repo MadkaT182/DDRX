df9ee0af27979d3aee18369fb4d2c6e54a32a745
unknown <madcat182@gmail.com>
2015-06-03 18:57:10 -0500

    Removed item shadows from screen result

--- BGAnimations/ScreenSelectPlayCourseMode background/default.lua
+++ BGAnimations/ScreenSelectPlayCourseMode background/default.lua
@@ -2,13 +2,16 @@ return Def.ActorFrame {
 	LoadActor( "../_shared background" )..{
 	};
 
-	LoadActor( "../header" )..{
-		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-240;zoomx,1.34;vertalign,top;addy,-24;linear,0.133;addy,24);
+	LoadActor( "../Common ScreenStages/top_stage" )..{
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_TOP+12;zoomx,1.34;addy,-56;linear,0.133;addy,56);
 		OffCommand=cmd(linear,0.133;addy,-56);
 	};
 
-	LoadActor( "../headerframe" )..{
-		OnCommand=cmd(x,SCREEN_CENTER_X-159;y,SCREEN_CENTER_Y-232;vertalign,top;horizalign,right;zoom,0;sleep,0.283;linear,0.1;zoom,1.05;linear,0.033;zoom,1);
-		OffCommand=cmd(linear,0.133;addy,-62);
+	LoadActor( "../Common ScreenStages/bottom_stage" )..{
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_BOTTOM-12;zoomx,1.34;addy,24;linear,0.133;addy,-24);
+		OffCommand=cmd(linear,0.133;addy,24);
+	};
+
+	LoadActor("../../Graphics/timerBG")..{
 	};
 }
