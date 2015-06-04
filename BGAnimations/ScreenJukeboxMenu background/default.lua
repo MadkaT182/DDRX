d6915bd5eac4c87dbdbfd57a362e4c5dace59c75
unknown <madcat182@gmail.com>
2015-06-04 09:58:00 -0500

    Updated path of file for jukebox bg

--- BGAnimations/ScreenJukeboxMenu background/default.lua
+++ BGAnimations/ScreenJukeboxMenu background/default.lua
@@ -1,5 +1,5 @@
 return Def.ActorFrame {
-	LoadActor( "BGA" )..{
+	LoadActor( "../_shared options background/BGA" )..{
 		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoomtowidth,SCREEN_WIDTH;zoomtoheight,SCREEN_HEIGHT);
 	};
 
