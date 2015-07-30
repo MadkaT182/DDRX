24f8347c638cc310b19d8bd8434aa0ea58a4bb77
unknown <madcat182@gmail.com>
2015-07-30 18:49:19 -0500

    Removed elements from default theme

--- BGAnimations/ScreenWithMenuElements decorations/default.lua
+++ BGAnimations/ScreenWithMenuElements decorations/default.lua
@@ -2,7 +2,7 @@
 -- I prefer to keep these optional, incase another screen wants to hide 
 -- these elements.
 local t = Def.ActorFrame {};
-t[#t+1] = StandardDecorationFromFileOptional("Header","Header");
-t[#t+1] = StandardDecorationFromFileOptional("Footer","Footer");
-t[#t+1] = StandardDecorationFromFileOptional( "Help", "Help" );
+-- t[#t+1] = StandardDecorationFromFileOptional("Header","Header");
+-- t[#t+1] = StandardDecorationFromFileOptional("Footer","Footer");
+-- t[#t+1] = StandardDecorationFromFileOptional( "Help", "Help" );
 return t
