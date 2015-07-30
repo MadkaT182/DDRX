6dd1ed090697889696b7fd70e492d6f16a47744a
unknown <madcat182@gmail.com>
2015-07-30 18:43:52 -0500

    Removed code

--- BGAnimations/ScreenProfileLoad overlay.lua
+++ BGAnimations/ScreenProfileLoad overlay.lua
@@ -1,15 +1,4 @@
-local x = Def.ActorFrame{
-	-- Def.Quad{
-	-- 	InitCommand=cmd(Center;zoomto,SCREEN_WIDTH,80;diffuse,color("0,0,0,0.5"));
-	-- 	OnCommand=cmd();
-	-- 	OffCommand=cmd();
-	-- };
-	-- LoadFont("Common Normal")..{
-	-- 	Text=ScreenString("Loading Profiles");
-	-- 	InitCommand=cmd(Center;diffuse,color("1,1,1,1");shadowlength,1);
-	-- 	OffCommand=cmd(linear,0.15;diffusealpha,0);
-	-- };
-};
+local x = Def.ActorFrame{};
 
 x[#x+1] = Def.Actor {
 	BeginCommand=function(self)
