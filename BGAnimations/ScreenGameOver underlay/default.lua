fecf2949e120757abf682b8e6db38dbb8d56a490
unknown <madcat182@gmail.com>
2015-08-19 11:50:39 -0500

    Fixed Game Over BG

--- BGAnimations/ScreenGameOver underlay/default.lua
+++ BGAnimations/ScreenGameOver underlay/default.lua
@@ -1,26 +1,13 @@
 local timer_seconds = THEME:GetMetric(Var "LoadingScreen","TimerSeconds");
 
 return Def.ActorFrame {
-	InitCommand=cmd(Center),
-	-- Fade
-	Def.ActorFrame {
-		InitCommand=cmd();
-		Def.Quad {
-			InitCommand=cmd(scaletoclipped,SCREEN_WIDTH,SCREEN_HEIGHT),
-			OnCommand=cmd(diffuse,Color.Black;diffusealpha,0;linear,0.5;diffusealpha,0.25;
-						sleep,timer_seconds/2;  
-						linear,timer_seconds/2-0.5;diffusealpha,0.8),
-		},
-		-- Warning Fade
-		Def.Quad {
-			InitCommand=cmd(y,-4;scaletoclipped,SCREEN_WIDTH,164),
-			OnCommand=cmd(diffuse,Color.Black;diffusealpha,0.5;
-						  linear,timer_seconds;zoomtoheight,164*0.75),
-		}
-	},
+
+	LoadActor( "../_black" )..{
+		InitCommand=cmd(x,SCREEN_LEFT;y,SCREEN_TOP;FullScreen);
+	};
 	--
 	LoadActor(THEME:GetPathG("ScreenGameOver","gameover"))..{
-		InitCommand=cmd(y,-16;shadowlength,2)
-	},
+		InitCommand=cmd(y,-16;shadowlength,2;Center);
+	};
 
 }
