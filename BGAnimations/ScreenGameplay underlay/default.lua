96236398428252f6ce95c87e716e992afab6e741
unknown <madcat182@gmail.com>
2015-08-19 11:48:04 -0500

    Added difficulty frame to gameplay screen

--- BGAnimations/ScreenGameplay underlay/default.lua
+++ BGAnimations/ScreenGameplay underlay/default.lua
@@ -3,19 +3,28 @@ local t = Def.ActorFrame{
 	LoadActor("ScreenFilter");
 };
 
--- Score frame
+-- Score decorations P1
 if GAMESTATE:IsPlayerEnabled(PLAYER_1) then
 	t[#t+1] = Def.ActorFrame{
 		LoadActor("score_frame")..{
 			OnCommand=cmd(x,SCREEN_LEFT+98;y,SCREEN_BOTTOM-38);
 		};
+
+		LoadActor("dif_frame")..{
+			OnCommand=cmd(x,SCREEN_LEFT+71;y,SCREEN_BOTTOM-56);
+		};
 	}
 end
 
+-- Score decorations P2
 if GAMESTATE:IsPlayerEnabled(PLAYER_2) then
 	t[#t+1] = Def.ActorFrame{
 		LoadActor("score_frame")..{
 			OnCommand=cmd(rotationy,180;x,SCREEN_RIGHT-98;y,SCREEN_BOTTOM-38);
+		};
+
+		LoadActor("dif_frame")..{
+			OnCommand=cmd(rotationy,180;x,SCREEN_RIGHT-71;y,SCREEN_BOTTOM-56);
 		};
 	}
 end
