52656006039a6cbbcff20b77dfea2d85f929f1de
unknown <madcat182@gmail.com>
2015-08-18 22:17:41 -0500

    Added score frame to gameplay screen

--- BGAnimations/ScreenGameplay underlay/default.lua
+++ BGAnimations/ScreenGameplay underlay/default.lua
@@ -3,4 +3,21 @@ local t = Def.ActorFrame{
 	LoadActor("ScreenFilter");
 };
 
+-- Score frame
+if GAMESTATE:IsPlayerEnabled(PLAYER_1) then
+	t[#t+1] = Def.ActorFrame{
+		LoadActor("score_frame")..{
+			OnCommand=cmd(x,SCREEN_LEFT+98;y,SCREEN_BOTTOM-38);
+		};
+	}
+end
+
+if GAMESTATE:IsPlayerEnabled(PLAYER_2) then
+	t[#t+1] = Def.ActorFrame{
+		LoadActor("score_frame")..{
+			OnCommand=cmd(rotationy,180;x,SCREEN_RIGHT-98;y,SCREEN_BOTTOM-38);
+		};
+	}
+end
+
 return t;
