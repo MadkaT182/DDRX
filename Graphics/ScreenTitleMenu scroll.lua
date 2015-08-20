2a35b439e55fbc62fa58385c140611af5a83bf79
unknown <madcat182@gmail.com>
2015-08-19 19:37:22 -0500

    Removed bg shadows from title screen options

--- Graphics/ScreenTitleMenu scroll.lua
+++ Graphics/ScreenTitleMenu scroll.lua
@@ -1,12 +1,6 @@
 local gc = Var("GameCommand");
 
 return Def.ActorFrame {
-	Def.Quad{
-		InitCommand=cmd(zoomto,256,26;fadeleft,0.45;faderight,0.45);
-		OnCommand=cmd(diffuseshift;effectcolor1,color("0,0,0,0.5");effectcolor2,color("0,0,0,0.5"));
-		GainFocusCommand=cmd(stoptweening;decelerate,0.1;zoomto,256,26;diffusealpha,1);
-		LoseFocusCommand=cmd(stoptweening;accelerate,0.1;zoomto,SCREEN_WIDTH,0;diffusealpha,0);
-	};
 	LoadFont("ScreenTitleMenu choices") .. {
 		Text=THEME:GetString("ScreenTitleMenu",gc:GetText());
 		OnCommand=cmd(shadowlength,1);
