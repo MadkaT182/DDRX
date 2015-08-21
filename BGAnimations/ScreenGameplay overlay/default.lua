da5faa3437fd9d4b8cd09abd2f73eb721900b6db
unknown <madcat182@gmail.com>
2015-08-21 12:06:55 -0500

    Added stage bgframe

--- BGAnimations/ScreenGameplay overlay/default.lua
+++ BGAnimations/ScreenGameplay overlay/default.lua
@@ -10,6 +10,7 @@ t[#t+1] = LoadActor( "../../Graphics/ScreenGameplay song info" )..{
 	InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_BOTTOM-42;zoom,1);
 };
 
+t[#t+1] = StandardDecorationFromFileOptional("StageFrame","StageFrame");
 t[#t+1] = StandardDecorationFromFileOptional("StageDisplay","StageDisplay");
 t[#t+1] = StandardDecorationFromFileOptional("SongTitle","SongTitle");
 
