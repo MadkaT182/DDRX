f170b30530c451c09733a208e9ba4fbdeb9750c6
unknown <madcat182@gmail.com>
2017-01-04 11:20:19 -0500

    Added demonstration behaviour

--- BGAnimations/_stageFrame/default.lua
+++ BGAnimations/_stageFrame/default.lua
@@ -1,21 +1,25 @@
 local t = Def.ActorFrame {};
 
-if GAMESTATE:GetPlayMode() == 'PlayMode_Rave' then
-	t[#t+1] = LoadActor("RaveFrame")..{
-		OnCommand=cmd(x,SCREEN_CENTER_X);
-	};
-elseif GAMESTATE:GetPlayMode() == 'PlayMode_Oni' or GAMESTATE:GetPlayMode() == 'PlayMode_Nonstop' then
-	t[#t+1] = LoadActor("CourseFrame")..{
-		OnCommand=cmd(x,SCREEN_CENTER_X);
-	};
-else
-	t[#t+1] = LoadActor("StageFrame")..{
-		OnCommand=cmd(x,SCREEN_CENTER_X);
-	};
-end;
+if not GAMESTATE:IsDemonstration() then
 
-t[#t+1] = LoadActor("../_gpstg")..{
-	OnCommand=cmd(x,SCREEN_CENTER_X+8;y,SCREEN_TOP+6);
-};
+	if GAMESTATE:GetPlayMode() == 'PlayMode_Rave' then
+		t[#t+1] = LoadActor("RaveFrame")..{
+			OnCommand=cmd(x,SCREEN_CENTER_X);
+		};
+	elseif GAMESTATE:GetPlayMode() == 'PlayMode_Oni' or GAMESTATE:GetPlayMode() == 'PlayMode_Nonstop' then
+		t[#t+1] = LoadActor("CourseFrame")..{
+			OnCommand=cmd(x,SCREEN_CENTER_X);
+		};
+	else
+		t[#t+1] = LoadActor("StageFrame")..{
+			OnCommand=cmd(x,SCREEN_CENTER_X);
+		};
+	end;
+
+	t[#t+1] = LoadActor("../_gpstg")..{
+		OnCommand=cmd(x,SCREEN_CENTER_X+8;y,SCREEN_TOP+6);
+	};
+
+end
 
 return t;
