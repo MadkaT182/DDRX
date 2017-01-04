6d9b12f1da6a229370d739aca6d70b423be4d40d
unknown <madcat182@gmail.com>
2017-01-04 16:38:02 -0500

    Fixed paths

--- BGAnimations/ScreenGameplay failed/default.lua
+++ BGAnimations/ScreenGameplay failed/default.lua
@@ -20,27 +20,27 @@ LoadActor("../door2")..{
 OnCommand=cmd(x,SCREEN_CENTER_X+SCREEN_WIDTH/4;y,SCREEN_CENTER_Y;addx,SCREEN_WIDTH/2;sleep,0.000;sleep,0.316;linear,0.2;addx,-SCREEN_WIDTH/2;sleep,1.167;sleep,3.383;linear,0.2;addx,SCREEN_WIDTH/2);
 };
 
-LoadActor("../ScreenGameplay Cleared/s01")..{
+LoadActor("../ScreenGameplay out/s01")..{
 OnCommand=cmd(x,SCREEN_CENTER_X+188;y,SCREEN_CENTER_Y-115;diffusealpha,0;sleep,0.000;sleep,0.766;diffusealpha,1;sleep,4.3;linear,0.05;diffusealpha,0);
 };
 
-LoadActor("../ScreenGameplay Cleared/s02")..{
+LoadActor("../ScreenGameplay out/s02")..{
 OnCommand=cmd(x,SCREEN_CENTER_X-204;y,SCREEN_CENTER_Y+48;diffusealpha,0;sleep,0.000;sleep,0.833;diffusealpha,1;sleep,4.233;linear,0.05;diffusealpha,0);
 };
 
-LoadActor("../ScreenGameplay Cleared/s01")..{
+LoadActor("../ScreenGameplay out/s01")..{
 OnCommand=cmd(x,SCREEN_CENTER_X-104;y,SCREEN_CENTER_Y+109;diffusealpha,0;sleep,0.000;sleep,0.883;diffusealpha,1;sleep,4.183;linear,0.05;diffusealpha,0);
 };
 
-LoadActor("../ScreenGameplay Cleared/s03")..{
+LoadActor("../ScreenGameplay out/s03")..{
 OnCommand=cmd(x,SCREEN_CENTER_X+96;y,SCREEN_CENTER_Y+22;diffusealpha,0;sleep,0.000;sleep,0.899;diffusealpha,1;sleep,4.167;linear,0.05;diffusealpha,0);
 };
 
-LoadActor("../ScreenGameplay Cleared/s04")..{
+LoadActor("../ScreenGameplay out/s04")..{
 OnCommand=cmd(x,SCREEN_CENTER_X+186;y,SCREEN_CENTER_Y+20;diffusealpha,0;sleep,0.000;sleep,0.933;diffusealpha,1;sleep,4.133;linear,0.05;diffusealpha,0);
 };
 
-LoadActor("../ScreenGameplay Cleared/s05")..{
+LoadActor("../ScreenGameplay out/s05")..{
 OnCommand=cmd(x,SCREEN_CENTER_X+134;y,SCREEN_CENTER_Y-102;diffusealpha,0;sleep,0.000;sleep,0.966;diffusealpha,1;sleep,4.1;linear,0.05;diffusealpha,0);
 };
 
