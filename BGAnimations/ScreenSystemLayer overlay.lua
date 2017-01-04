d0e84d08c5363877bdb8de507ace69ad9700989f
unknown <madcat182@gmail.com>
2017-01-03 21:47:39 -0500

    Added credits footer

---
+++ BGAnimations/ScreenSystemLayer overlay.lua
@@ -0,0 +1,131 @@
+local function CreditsText()
+	local text = LoadFont("_sys1") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X-27;y,SCREEN_BOTTOM-12;playcommand,"Refresh");
+		RefreshCommand=function(self)
+		--Other coin modes
+			if GAMESTATE:IsEventMode() then self:settext('EVENT MODE') return end
+			if GAMESTATE:GetCoinMode()=='CoinMode_Free' then self:settext('FREE PLAY') return end
+			if GAMESTATE:GetCoinMode()=='CoinMode_Home' then self:settext('') return end
+		--Normal pay
+			local coins=GAMESTATE:GetCoins()
+			local coinsPerCredit=PREFSMAN:GetPreference('CoinsPerCredit')
+			local credits=math.floor(coins/coinsPerCredit)
+			local remainder=math.mod(coins,coinsPerCredit)
+			local s='CREDIT:'
+			if credits > 1 then
+				s='CREDITS:'..credits
+			elseif credits == 1 then
+				s=s..credits
+			else
+				s=s..0
+			end
+			self:horizalign(left)
+			self:settext(s)
+		end;
+		UpdateVisibleCommand=function(self)
+			local screen = SCREENMAN:GetTopScreen();
+			local bShow = true;
+			if screen then
+				local sClass = screen:GetName();
+				bShow = THEME:GetMetric( sClass, "ShowCreditDisplay" );
+			end;
+
+			self:visible( bShow );
+		end;
+		CoinInsertedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		RefreshCreditTextMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		PlayerJoinedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		ScreenChangedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+	};
+	return text;
+end;
+
+local function CoinsText()
+	local text = LoadFont("_sys1") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X+281;y,SCREEN_BOTTOM-12;horizalign,center;playcommand,"Refresh");
+		RefreshCommand=function(self)
+			local coins=GAMESTATE:GetCoins()
+			local coinsPerCredit=PREFSMAN:GetPreference('CoinsPerCredit')
+			local remainder=math.mod(coins,coinsPerCredit)
+			local s='COIN(S):'
+			if coinsPerCredit > 1 then
+				s=s..remainder..'/'..coinsPerCredit
+			else
+				s=''
+			end
+
+			if GAMESTATE:GetCoinMode() == 'CoinMode_Pay' then
+				self:visible(true);
+			else
+				self:visible(false);
+			end
+
+			self:settext(s)
+		end;
+		UpdateVisibleCommand=function(self)
+			local screen = SCREENMAN:GetTopScreen();
+			local bShow = true;
+			if screen then
+				local sClass = screen:GetName();
+				bShow = THEME:GetMetric( sClass, "ShowCreditDisplay" );
+			end;
+
+			self:visible( bShow );
+		end;
+		CoinInsertedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		RefreshCreditTextMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		PlayerJoinedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		ScreenChangedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+	};
+	return text;
+end;
+
+local function NetworkText()
+	local text = LoadFont("_sys1") .. {
+		InitCommand=function (self)
+			self:name("NetworkStatus");
+			self:settext("-----");
+			self:x(SCREEN_CENTER_X-103);
+			self:y(SCREEN_BOTTOM-12);
+			self:horizalign(right);
+		end;
+		RefreshCommand=function (self)
+		local netConnected = IsNetConnected();
+		local loggedOnSMO = IsNetSMOnline();
+			if netConnected then
+				self:diffuse(color("#00FFFF"));
+				self:settext("ONLINE");
+			else
+				self:diffuse(color("#999999"));
+				self:settext("LOCAL MODE");
+			end;
+		end;
+		UpdateVisibleCommand=function(self)
+			local screen = SCREENMAN:GetTopScreen();
+			local bShow = true;
+			if screen then
+				local sClass = screen:GetName();
+				bShow = THEME:GetMetric( sClass, "ShowCreditDisplay" );
+			end
+
+			self:visible( bShow );
+		end;
+		CoinInsertedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		RefreshCreditTextMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		PlayerJoinedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		ScreenChangedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+	};
+	return text;
+end;
+
+local t = Def.ActorFrame {}
+
+t[#t+1] = Def.ActorFrame {
+ 	--CreditsText( PLAYER_1 );
+	--CreditsText( PLAYER_2 );
+	NetworkText();
+	CreditsText();
+	CoinsText();
+};
+
+return t;
