f2701c54bf3620f0747d589248c3cbdfb9b806cf
unknown <madcat182@gmail.com>
2017-01-06 10:17:15 -0500

    Code cleaning

--- BGAnimations/ScreenCaution background/default.lua
+++ BGAnimations/ScreenCaution background/default.lua
@@ -1,8 +1,4 @@
 return Def.ActorFrame {
-	LoadActor( "../_black" )..{
-		OnCommand=cmd();
-	};
-
 	LoadActor( "caution" )..{
 		OnCommand=cmd(Center;FullScreen;diffusealpha,0;decelerate,0.3;diffusealpha,1;sleep,2.4;accelerate,0.3;diffusealpha,0);
 	};
